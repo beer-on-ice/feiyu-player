c3d84fd3c280f1ba09a32bd6228a1c37e980e447
啤酒加冰 <e@mail.666986.xyz>
2025-05-19 15:56:45 +0800

    Update default.ts

--- packages/feiyu/src/data/default.ts
+++ packages/feiyu/src/data/default.ts
@@ -4,6 +4,110 @@ export default {
   proxy: 'https://feiyu-proxy.666986.xyz/api/proxy',
   videoSources: [
     {
+      "key": "电影天堂资源",
+      "api": "http://caiji.dyttzyapi.com/api.php/provide/vod"
+    },
+    {
+      "key": "如意资源",
+      "api": "https://cj.rycjapi.com/api.php/provide/vod"
+    },
+    {
+      "key": "金蝉资源",
+      "api": "http://jczy.jinchan.pro/api.php/provide/vod"
+    },
+    {
+      "key": "无线资源",
+      "api": "https://api.wuxianzy.net/api.php/provide/vod"
+    },
+    {
+      "key": "番茄资源",
+      "api": "https://fqzy.me/api.php/provide/vod"
+    },
+    {
+      "key": "麒麟资源",
+      "api": "https://www.qilinzyz.com/api.php/provide/vod"
+    },
+    {
+      "key": "魔爪资源",
+      "api": "https://mozhuazy.com/api.php/provide/vod"
+    },
+    {
+      "key": "暴风资源",
+      "api": "https://bfzyapi.com/api.php/provide/vod"
+    },
+    {
+      "key": "天涯资源",
+      "api": "https://tyyszy.com/api.php/provide/vod"
+    },
+    {
+      "key": "非凡影视",
+      "api": "http://ffzy5.tv/api.php/provide/vod"
+    },
+    {
+      "key": "黑木耳",
+      "api": "https://json.heimuer.xyz/api.php/provide/vod"
+    },
+    {
+      "key": "360资源",
+      "api": "https://360zy.com/api.php/provide/vod"
+    },
+    {
+      "key": "iqiyi资源",
+      "api": "https://www.iqiyizyapi.com/api.php/provide/vod"
+    },
+    {
+      "key": "卧龙资源",
+      "api": "https://wolongzyw.com/api.php/provide/vod"
+    },
+    {
+      "key": "华为吧资源",
+      "api": "https://cjhwba.com/api.php/provide/vod"
+    },
+    {
+      "key": "茅台资源",
+      "api": "https://caiji.maotaizy.cc/api.php/provide/vod"
+    },
+    {
+      "key": "X细胞资源",
+      "api": "https://www.xxibaozyw.com/api.php/provide/vod"
+    },
+    {
+      "key": "极速资源",
+      "api": "https://jszyapi.com/api.php/provide/vod"
+    },
+    {
+      "key": "豆瓣资源",
+      "api": "https://dbzy.com/api.php/provide/vod"
+    },
+    {
+      "key": "魔都资源",
+      "api": "https://www.mdzyapi.com/api.php/provide/vod"
+    },
+    {
+      "key": "最大资源",
+      "api": "https://api.zuidapi.com/api.php/provide/vod"
+    },
+    {
+      "key": "樱花资源",
+      "api": "https://m3u8.apiyhzy.com/api.php/provide/vod"
+    },
+    {
+      "key": "百度云资源",
+      "api": "https://api.apibdzy.com/api.php/provide/vod"
+    },
+    {
+      "key": "无尽资源",
+      "api": "https://api.wujinapi.me/api.php/provide/vod"
+    },
+    {
+      "key": "旺旺短剧",
+      "api": "https://wwzy.tv/api.php/provide/vod"
+    },
+    {
+      "key": "iKun资源",
+      "api": "https://ikunzyapi.com/api.php/provide/vod"
+    },
+    {
       "key": "CK资源",
       "api": "https://www.ckzy1.com/api.php/provide/vod/at/xml"
     },
@@ -74,12 +178,12 @@ export default {
   ],
   hotMovies: [
     {
-      id: '35588177',
+      id: '25848328',
       isNew: false,
-      title: '漫长的季节',
+      title: '最后生还者 第一季',
       cover:
-        'https://img9.doubanio.com/view/photo/s_ratio_poster/public/p2890906384.jpg',
-      rate: '9.4',
-    },
+        'https://img9.doubanio.com/view/photo/s_ratio_poster/public/p2884221114.jpg',
+      rate: '9.1',
+    }
   ],
 } as FeiyuConfig;
