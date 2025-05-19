5f2a2bbd7d28e860424d9845c9e0492ec2bf8f13
beer-on-ice <li997477295@outlook.com>
2025-05-19 14:46:51 +0800

    fix

--- packages/feiyu/src/data/default.ts
+++ packages/feiyu/src/data/default.ts
@@ -4,21 +4,73 @@ export default {
   proxy: 'https://feiyu-proxy.666986.xyz/api/proxy',
   videoSources: [
     {
-      "key": "视频源",
-      "api": "https://fqzy.me/api.php/provide/vod/at/xml"
+      "key": "CK资源",
+      "api": "https://www.ckzy1.com/api.php/provide/vod/at/xml"
     },
     {
-        "key": "视频源2",
-        "api": "https://mozhuazy.com/api.php/provide/vod/at/xml"
+      "key": "jkun资源",
+      "api": "https://jkunzyapi.com/api.php/provide/vod/at/xml"
     },
     {
-        "key": "视频源3",
-        "api": "https://json.heimuer.xyz/api.php/provide/vod/at/xml"
+      "key": "百万资源",
+      "api": "https://api.bwzym3u8.com/api.php/provide/vod/at/xml"
     },
     {
-        "key": "视频源4",
-        "api": "https://cjhwba.com/api.php/provide/vod/at/xml"
+      "key": "souav资源",
+      "api": "https://api.souavzy.vip/api.php/provide/vod/at/xml"
     },
+    {
+      "key": "155资源",
+      "api": "https://155api.com/api.php/provide/vod/at/xml"
+    },
+    {
+      "key": "lsb资源",
+      "api": "https://apilsbzy1.com/api.php/provide/vod/at/xml"
+    },
+    {
+      "key": "黄色仓库",
+      "api": "https://hsckzy.vip/api.php/provide/vod/at/xml"
+    },
+    {
+      "key": "玉兔资源",
+      "api": "https://yutuzy10.com/api.php/provide/vod/at/xml"
+    },
+    {
+      "key": "杏吧资源",
+      "api": "https://xingba111.com/api.php/provide/vod/at/xml"
+    },
+    {
+      "key": "小鸡资源",
+      "api": "https://api.xiaojizy.live/api.php/provide/vod/at/xml"
+    },
+    {
+      "key": "极品X资源",
+      "api": "https://jipinzyapi.com/api.php/provide/vod/at/xml"
+    },
+    {
+      "key": "丝袜资源",
+      "api": "https://siwazyw.tv/api.php/provide/vod/at/xml"
+    },
+    {
+      "key": "色猫资源",
+      "api": "https://caiji.semaozy.net/api.php/provide/vod/at/xml"
+    },
+    {
+      "key": "易看资源",
+      "api": "https://api.ekanapi.com/api.php/provide/vod/at/xml"
+    },
+    {
+      "key": "奶香香资源",
+      "api": "https://naixxzy.com/api.php/provide/vod/at/xml"
+    },
+    {
+      "key": "辣椒资源",
+      "api": "https://apilj.com/api.php/provide/vod/at/xml"
+    },
+    {
+      "key": "大地资源",
+      "api": "https://dadiapi.com/api.php/provide/vod/at/xml"
+    }
   ],
   hotMovies: [
     {
